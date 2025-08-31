3044aa3b0b4aef7a4c84ba8e62c6577754cf586a
Hugh Sorby <h.sorby@auckland.ac.nz>
2025-09-01 11:22:51 +1200

    Set version to 0.7.3.

--- mapclientplugins/argonsceneexporterstep/__init__.py
+++ mapclientplugins/argonsceneexporterstep/__init__.py
@@ -3,7 +3,7 @@
 MAP Client Plugin
 """
 
-__version__ = '0.7.2'
+__version__ = '0.7.3'
 __author__ = 'Kay Wang'
 __stepname__ = 'Argon Scene Exporter'
 __location__ = 'https://github.com/mapclient-plugins/mapclientplugins.argonsceneexporterstep'
